2bcc8d3f9fd8b9b3a330070a43fd632363599cbd
Rafael Neves <rafael.neves@unbabel.com>
2024-02-15 10:05:29 +0000

    fixed configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { ConfigEnv, defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/variables.scss";`,
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const cwd = process.cwd();
+  const env = { ...loadEnv(mode, cwd, "VITE_") };
+
+  return {
+    plugins: [vue()],
+    server: { port: Number(env?.VITE_PORT ?? 3000) },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/assets/variables.scss";`,
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
